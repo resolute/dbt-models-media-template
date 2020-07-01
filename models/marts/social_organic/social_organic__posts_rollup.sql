cc659d8b604c068fafb23a0add7a27b6138f9cb7
John Allen <john.allen@resolute.com>
2020-07-01 21:05:06 +0000

    Refactors both social paid and organic rollup models

--- models/marts/social_organic/social_organic__posts_rollup.sql
+++ models/marts/social_organic/social_organic__posts_rollup.sql
@@ -1,29 +1,18 @@
-{# Create dictionary variable of upstream models that could be used in the rollup #}
-{%- 
-set upstream_models = {
-    'facebook': {
-        'has_data': var('facebook_organic_ids') != None,
-        'model_ref': 'facebook_organic__posts_lifetime'
-    },
-    'instagram': {
-        'has_data': var('instagram_organic_ids') != None,
-        'model_ref': 'instagram_organic__posts_lifetime'
-    },
-    'twitter': {
-        'has_data': var('twitter_organic_ids') != None,
-        'model_ref': 'twitter_organic__posts_lifetime'
-    },
-    'linkedin': {
-        'has_data': var('linkedin_organic_ids') != None,
-        'model_ref': 'linkedin_organic__posts_lifetime'
-    }
-}
--%}
+{#- Create list variable of upstream models that could be used in the rollup -#}
+{%- set upstream_models = ['facebook_organic__posts_lifetime', 'instagram_organic__posts_lifetime', 'twitter_organic__posts_lifetime', 'twitter_organic__posts_lifetime'] -%}
 
-{# Enable this model only if any of the upstream models have data #}
+{#- Create list variable of enabled upstream models relations -#}
+{% set enabled_upstream_models = [] -%}
+{% for node in graph.nodes.values() -%}
+    {%- if node.name in upstream_models and node.config.enabled -%}
+        {%- do enabled_upstream_models.append(ref(node.name)) -%}
+    {%- endif -%}
+{%- endfor %}
+
+{#- Enable this model only if any of the upstream models are enabled -#}
 {{-
     config(
-        enabled = (upstream_models["facebook"].has_data or upstream_models["instagram"].has_data or upstream_models["twitter"].has_data or upstream_models["linkedin"].has_data) == true
+        enabled = enabled_upstream_models|length > 0
     )
 -}}
 
@@ -31,14 +20,10 @@ WITH
 
 union_tables AS (
 
-    {# Loop through each upstream model that has data from the dictionary variable and generate a union statement #}
-    {%- for item in upstream_models if upstream_models[item].has_data  -%}
-
-        SELECT * FROM {{ ref(upstream_models[item].model_ref) }}
-
-        {% if not loop.last %} UNION ALL {% endif %}
-
-    {%- endfor -%}
+{{ dbt_utils.union_relations(
+    relations= enabled_upstream_models,
+    exclude= ['id']
+) }}
 
 ),
 
--- models/marts/social_paid/social_paid__ads_rollup.sql
+++ models/marts/social_paid/social_paid__ads_rollup.sql
@@ -1,64 +1,40 @@
+{#- Create list variable of upstream models that could be used in the rollup -#}
+{%- set upstream_models = ['facebook_ads__creatives_daily', 'linkedin_ads__creatives_daily'] -%}
+
+{#- Create list variable of enabled upstream models relations -#}
+{% set enabled_upstream_models = [] -%}
+{% for node in graph.nodes.values() -%}
+    {%- if node.name in upstream_models and node.config.enabled -%}
+        {%- do enabled_upstream_models.append(ref(node.name)) -%}
+    {%- endif -%}
+{%- endfor %}
+
+{#- Enable this model only if any of the upstream models are enabled -#}
+{{-
+    config(
+        enabled = enabled_upstream_models|length > 0
+    )
+-}}
+
 WITH
 
---Facebook Paid
-fb_paid AS (
-    
-    SELECT * FROM {{ ref('facebook_ads__creatives_daily') }}
-
-),
-/*    
---Instagram Paid
-ig_paid AS (
-
-    SELECT * FROM `yale-opac.instagram_organic.int_instagram_organic_post_lifetime`
-
-),
-  
---Twitter Paid
-tw_paid AS (
-
-    SELECT * FROM `yale-opac.twitter_organic.int_twitter_organic_tweets_lifetime`
-
-),
-
---LinkedIn Paid
-li_paid AS (
-
-    SELECT * FROM `yale-opac.linkedin_organic.int_linkedin_organic_share_lifetime`
-
-),
-*/
---Union all social networks
-  
 union_tables AS (
-  
-    SELECT * FROM fb_paid
-    /*
-    UNION ALL
-    
-    SELECT * FROM ig_paid
-    
-    UNION ALL
-    
-    SELECT * FROM tw_paid
-    
-    UNION ALL
-    
-    SELECT * FROM li_paid
-    */
+
+{{ dbt_utils.union_relations(
+    relations= enabled_upstream_models,
+    exclude= ['id']
+) }}
+
 ),
 
 final AS (
 
     SELECT
-
+    
         {{ dbt_utils.surrogate_key(['date', 'account_id', 'creative_id']) }} AS id,
         *
-
-        EXCEPT(id)
     
     FROM union_tables
-
 )
 
 SELECT * FROM final
