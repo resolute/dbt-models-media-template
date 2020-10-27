a2d52180cc2065cd2891778ae82b6317bfaed2e8
John Allen <john.allen@resolute.com>
2020-10-27 21:17:25 +0000

    Integrated Pinterest Ads Pins data

--- models/marts/social_paid/intermediate/union_rollup_social_paid__campaigns.sql
+++ models/marts/social_paid/intermediate/union_rollup_social_paid__campaigns.sql
@@ -1,3 +1,7 @@
 {{ dbt_utils.union_relations(
-    relations= [ref('prep_rollup_social_paid__campaigns_facebook_ads'), ref('prep_rollup_social_paid__campaigns_linkedin_ads')]
+    relations= [
+        ref('prep_rollup_social_paid__campaigns_facebook_ads'),
+        ref('prep_rollup_social_paid__campaigns_linkedin_ads'),
+        ref('prep_rollup_social_paid__campaigns_pinterest_ads')
+    ]
 ) }}
