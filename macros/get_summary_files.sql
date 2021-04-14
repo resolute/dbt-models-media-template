72bba3e736c80b50dcb84c51ed62464f700e65df
John Allen <john.allen@resolute.com>
2021-04-14 20:12:52 +0000

    Fixed get_summary_files macro. Small updates to README file.

--- macros/get_summary_files.sql
+++ macros/get_summary_files.sql
@@ -6,11 +6,11 @@
     {% set _ = files.append(ref('prep_rollup_paid_media__campaigns_social_paid')) %}
     {% endif %}
 
-    {% if get_social_paid_files()|length > 0 %} 
+    {% if get_display_files()|length > 0 %} 
     {% set _ = files.append(ref('prep_rollup_paid_media__campaigns_display')) %}
     {% endif %}
 
-    {% if get_social_paid_files()|length > 0 %} 
+    {% if get_search_paid_files()|length > 0 %} 
     {% set _ = files.append(ref('prep_rollup_paid_media__campaigns_search_paid')) %}
     {% endif %}
 
