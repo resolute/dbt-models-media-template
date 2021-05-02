1752d7ee673650e150e5c681ea52acd9573892de
John Allen <john.allen@resolute.com>
2021-05-02 18:11:24 +0000

    Fixed refs in get_social_organic_files macro

--- macros/get_social_organic_files.sql
+++ macros/get_social_organic_files.sql
@@ -4,33 +4,33 @@
 
     {% if var('facebook_organic_ids')|length > 0 %} 
         {% if table_type == 'followers' %}
-            {% set _ = files.append(ref('facebook_organic__followers_daily')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__followers_daily_facebook')) %}
         {% elif table_type == 'posts' %}
-            {% set _ = files.append(ref('facebook_organic__posts_lifetime')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__posts_lifetime_facebook')) %}
         {% endif %}
     {% endif %}
 
     {% if var('instagram_organic_ids')|length > 0 %} 
         {% if table_type == 'followers' %}
-            {% set _ = files.append(ref('instagram_organic__followers_daily')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__followers_daily_instagram')) %}
         {% elif table_type == 'posts' %}
-            {% set _ = files.append(ref('instagram_organic__posts_lifetime')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__posts_lifetime_instagram')) %}
         {% endif %}
     {% endif %}
 
     {% if var('twitter_organic_ids')|length > 0 %} 
         {% if table_type == 'followers' %}
-            {% set _ = files.append(ref('twitter_organic__followers_daily')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__followers_daily_twitter')) %}
         {% elif table_type == 'posts' %}
-            {% set _ = files.append(ref('twitter_organic__posts_lifetime')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__posts_lifetime_twitter')) %}
         {% endif %}
     {% endif %}
 
     {% if var('linkedin_organic_ids')|length > 0 %} 
         {% if table_type == 'followers' %}
-            {% set _ = files.append(ref('linkedin_organic__followers_daily')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__followers_daily_linkedin')) %}
         {% elif table_type == 'posts' %}
-            {% set _ = files.append(ref('linkedin_organic__posts_lifetime')) %}
+            {% set _ = files.append(ref('prep_rollup_social_organic__posts_lifetime_linkedin')) %}
         {% endif %}
     {% endif %}
 
