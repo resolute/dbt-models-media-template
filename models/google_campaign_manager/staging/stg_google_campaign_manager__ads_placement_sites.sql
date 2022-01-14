b0f18219454d18f4ce5b3b88be78e2729178c145
John Allen <55411821+jallen13@users.noreply.github.com>
2022-01-14 10:45:22 -0500

    Update stg_google_campaign_manager__ads_placement_sites.sql
    Added "asset" field to the surrogate key arguments for the id field.

--- models/google_campaign_manager/staging/stg_google_campaign_manager__ads_placement_sites.sql
+++ models/google_campaign_manager/staging/stg_google_campaign_manager__ads_placement_sites.sql
@@ -152,7 +152,7 @@ final AS (
   
     SELECT 
     
-        {{ dbt_utils.surrogate_key(['date', 'account_id', 'advertiser_id', 'site_id', 'placement_id', 'placement_size', 'ad_id', 'creative_id', 'platform_type', 'browser_platform', 'connection_type']) }} AS id,
+        {{ dbt_utils.surrogate_key(['date', 'account_id', 'advertiser_id', 'site_id', 'placement_id', 'placement_size', 'ad_id', 'creative_id', 'asset', 'platform_type', 'browser_platform', 'connection_type']) }} AS id,
         'Campaign Manager' AS data_source,
         'Campaign Manager' AS channel_source_name,
         'Paid' AS channel_source_type,
